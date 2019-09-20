382c99d07d5cad4c3f89fb4575d3f4eb31dad66d
Turo Nylund <turo.nylund@outlook.com>
2019-09-20 09:35:16 +0300

    Lisatty Laskin luokka

---
+++ Testiprojekti/Testiprojekti/Laskin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Testiprojekti
+{
+    public class Laskin
+    {
+        public Laskin()
+        {
+        }
+
+        public int Summa(int luku1, int luku2)
+        {
+            return luku1 + luku2;
+        }
+    }
+}
--- Testiprojekti/Testiprojekti/Program.cs
+++ Testiprojekti/Testiprojekti/Program.cs
@@ -6,7 +6,13 @@ namespace Testiprojekti
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Laskin");
+
+            var laskin = new Laskin();
+
+            Console.WriteLine("Tulosta lukujen 5 ja 4 summa.");
+            var summa = laskin.Summa(5, 4);
+            Console.WriteLine(summa);
         }
     }
 }
